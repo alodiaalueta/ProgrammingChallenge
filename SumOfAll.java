884b5d9f459c88afe611f18270a8555abe555c63
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-07-21 09:03:34 +0000

    done

---
+++ SumOfAll.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class SumOfAll {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("============================================================");
+        System.out.println("              ~ Sum Of All Integer Inputs ~");
+        System.out.println("============================================================");
+        System.out.println("INSTRUCTIONS: Just type all your numbers in one line with\n              SPACES in between. Hit ENTER to see the total.");
+        System.out.print("\nInput here >>> ");
+        String inputLine = scanner.nextLine();
+        System.out.print("\n");
+        String[] numbers = inputLine.trim().split("\\s+");
+        int sum = 0;
+
+        for (String numStr : numbers) {
+            try {
+                int number = Integer.parseInt(numStr);
+                sum += number;
+            } catch (NumberFormatException e) {
+                System.out.println(numStr + " IS INVALID ❌ ");
+            }
+        }
+
+        System.out.println("\nSum Of All Integer Inputs: " + sum+ "\n");
+        scanner.close();
+    }
+}
