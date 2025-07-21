a3bc382b7b5c1f255db150a0bd43930d57d1dad3
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-07-21 10:38:34 +0000

    done

--- CheckVowelsConsonants.java
+++ CheckVowelsConsonants.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
 
-public class CheckVowelsConsonants { 
+public class CheckVowelsConsonants {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("\n===================================");
+        System.out.println("  ~ CHECK VOWELS AND CONSONANTS ~");
+        System.out.println("===================================");
+
         System.out.print("Enter a word: ");
         String word = input.nextLine().toLowerCase();
 
         int vowels = 0, consonants = 0;
+        String vowelChars = "", consonantChars = "";
 
         for (char c : word.toCharArray()) {
             if (Character.isLetter(c)) {
                 if ("aeiou".indexOf(c) != -1) {
                     vowels++;
+                    vowelChars += c + " ";
                 } else {
                     consonants++;
+                    consonantChars += c + " ";
                 }
             }
         }
 
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
-         input.close();
+        System.out.println("\nRESULT...");
+        System.out.println("Vowels: " + vowels + " > " + vowelChars.trim());
+        System.out.println("Consonants: " + consonants + " > " + consonantChars.trim());
+        System.out.print("\n");
+        input.close();
     }
 }
